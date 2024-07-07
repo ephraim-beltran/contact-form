292ce8b2171b1690396b4dc424ab8bc42bc18c47
ephraim <github@beltran.cc>
2024-07-07 04:05:25 -0400

    fix(input validation): validate all required inputs more accurately

--- src/ContactForm.jsx
+++ src/ContactForm.jsx
@@ -3,23 +3,20 @@ import "./ContactForm.css";
 
 function ContactForm() {
     const [inputInvalid, setInputInvalid] = useState({});
-    const validate = ([name, value]) => {
-        if(value.length == 0) {
-            setInputInvalid(current => ({...current, [name]: true}));
-            console.warn(`Input ${name} is empty.`);
-            return;
-        }
-        setInputInvalid(current => ({...current, [name]: false, queryType: false, consent: false}));
+    const handleInvalid = (event) => {
+        event.preventDefault();
+        const target = event.target;
+        console.warn(`Invalid ${target.name} input`);
+        setInputInvalid((invalidInputs) => ({...invalidInputs, [target.name] : true}))
     }
     const handleSubmit = (event) => {
         event.preventDefault();
         const formData = new FormData(event.target);
-        const formElement = event.target.elements;
-        // Handle Form Data here
-        for (const entry of formData.entries()) {
-            validate(entry);
+        for (const [name, value] of formData.entries()) {
+            if(value !== undefined | value.length > 0) {
+                setInputInvalid((invalidInputs) => ({...invalidInputs, [name]: false}));
+            }
         }
-        console.log(formElement);
     }
     const errorMessage = (type) => {
         let message;
@@ -42,27 +39,27 @@ function ContactForm() {
         return message;
     }
     return (
-    <form id="contact-form" onSubmit={handleSubmit}>
+    <form id="contact-form" onSubmit={handleSubmit} onInvalidCapture={handleInvalid}>
         <h2 className="wide">Contact Us</h2>
         <label className={(inputInvalid.firstName?"invalid-input ":"") + "split block"}>
             <span className="required-input">First Name</span>
-            <input type="text" name="firstName" />
+            <input type="text" name="firstName" required={true} />
             {inputInvalid.firstName && errorMessage('text')}
         </label>
         <label className={(inputInvalid.lastName?"invalid-input ":"") + "split block"}>
             <span className="required-input">Last Name</span>
-            <input type="text" name="lastName" />
+            <input type="text" name="lastName" required={true} />
             {inputInvalid.lastName && errorMessage('text')}
         </label>
         <label className={(inputInvalid.email?"invalid-input ":"") + "wide block"}>
             <span className="required-input">Email Address</span>
-            <input type="email" name="email" />
+            <input type="email" name="email" required={true} />
             {inputInvalid.email && errorMessage('email')}
         </label>
         <fieldset className={(inputInvalid.queryType?"invalid-input ":"") + "wide"}>
             <span className="required-input wide">Query Type</span>
             <label className="split">
-                <input type="radio" name="queryType" value="general-enquiry" />
+                <input type="radio" name="queryType" value="general-enquiry" required={true} />
                 <span>General Inquiry</span>
             </label>
             <label className="split">
@@ -73,11 +70,11 @@ function ContactForm() {
         </fieldset>
         <label className={(inputInvalid.message?"invalid-input ":"") + "wide block"}>
             <span className="required-input">Message</span>
-            <textarea name="message"></textarea>
+            <textarea name="message" required={true}></textarea>
             {inputInvalid.message && errorMessage('text')}
         </label>
         <label className={(inputInvalid.consent?"invalid-input ":"") + "wide"}>
-            <input type="checkbox" name="consent" />
+            <input type="checkbox" name="consent" required={true} />
             <span className="required-input">I consent to being contacted by the team</span>
             {inputInvalid.consent && errorMessage('consent')}
         </label>
