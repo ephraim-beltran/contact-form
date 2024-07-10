342ba3c0d1c805a6b84e1ff1fee789ea33dfa66e
ephraim <github@beltran.cc>
2024-07-10 04:54:47 -0400

    refactor(input fields): move input fields into their own component

--- src/ContactForm.jsx
+++ src/ContactForm.jsx
@@ -1,95 +1,117 @@
 import { useState } from "react";
-import styles from "./ContactForm.module.css";
+import InputField from "./components/InputField";
 import "./ContactForm.css";
 
 function ContactForm() {
-    const [errorMessage, setErrorMessage] = useState({});
-    const setMessage = (inputName, message) => {
-        setErrorMessage(currentMessages => ({
-            ...currentMessages,
-            [inputName]: <span>{message}</span>
-        }))
-    }
-    const validate = (target) => {
-        const {validity, name} = target;
-        let inputType = 'input'
-        let emptyInputMessage = 'This field is required';
+  const [errorMessage, setErrorMessage] = useState({});
+  const setMessage = (inputName, message) => {
+    setErrorMessage((currentMessages) => ({
+      ...currentMessages,
+      [inputName]: <span>{message}</span>,
+    }));
+  };
+  const validate = (target) => {
+    const { validity, name } = target;
+    let inputType = "input";
+    let emptyInputMessage = "This field is required";
 
-        if(target.type == 'email') inputType = 'email address';
-        if(target.name == 'consent') emptyInputMessage = 'To submit this form, please consent to being contacted';
-        if(target.name == 'queryType') emptyInputMessage = 'Please select a query type';
+    if (target.type == "email") inputType = "email address";
+    if (target.name == "consent")
+      emptyInputMessage =
+        "To submit this form, please consent to being contacted";
+    if (target.name == "queryType")
+      emptyInputMessage = "Please select a query type";
 
-        if(validity.valid)              return setMessage(name, null);
+    if (validity.valid) return setMessage(name, null);
 
-        if(validity.badInput)           return setMessage(name, "Invalid input");
-        if(validity.patternMismatch)    return setMessage(name, "Invalid input");
-        if(validity.rangeOverflow)      return setMessage(name, "Input is higher than accepted value");
-        if(validity.rangeUnderflow)     return setMessage(name, "Input is lower than accepted value");
-        if(validity.stepMismatch)       return setMessage(name, "Invalid value");
-        if(validity.tooLong)            return setMessage(name, "Input is too long");
-        if(validity.tooShort)           return setMessage(name, "Input is too short");
-        if(validity.typeMismatch)       return setMessage(name, `Please entert a valid ${inputType}`);
-        if(validity.valueMissing)       return setMessage(name, emptyInputMessage);
-    }
-    const handleInvalid = (event) => {
-        event.preventDefault();
-        const target = event.target;
-        console.warn(`Invalid ${target.name} input`);
-        validate(target);
-    }
-    const handleChange = ({target}) => {
-        validate(target);
-    }
+    if (validity.badInput) return setMessage(name, "Invalid input");
+    if (validity.patternMismatch) return setMessage(name, "Invalid input");
+    if (validity.rangeOverflow)
+      return setMessage(name, "Input is higher than accepted value");
+    if (validity.rangeUnderflow)
+      return setMessage(name, "Input is lower than accepted value");
+    if (validity.stepMismatch) return setMessage(name, "Invalid value");
+    if (validity.tooLong) return setMessage(name, "Input is too long");
+    if (validity.tooShort) return setMessage(name, "Input is too short");
+    if (validity.typeMismatch)
+      return setMessage(name, `Please entert a valid ${inputType}`);
+    if (validity.valueMissing) return setMessage(name, emptyInputMessage);
+  };
+  const handleInvalid = (event) => {
+    event.preventDefault();
+    const target = event.target;
+    console.warn(`Invalid ${target.name} input`);
+    validate(target);
+  };
+  const handleChange = ({ target }) => {
+    validate(target);
+  };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const formData = new FormData(event.target);
-        // Handle form data here
-        console.dir(formData);
-    }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    // Handle form data here
+    console.dir(formData);
+  };
 
-    return (
-    <form id="contact-form" onSubmit={handleSubmit} onInvalidCapture={handleInvalid} onChangeCapture={handleChange}>
-        <h2 className="wide">Contact Us</h2>
-        <label className={"split block"}>
-            <span className="required-input">First Name</span>
-            <input type="text" name="firstName" required={true} />
-            {errorMessage.firstName}
-        </label>
-        <label className={"split block"}>
-            <span className="required-input">Last Name</span>
-            <input type="text" name="lastName" required={true} />
-            {errorMessage.lastName}
-        </label>
-        <label className={"wide block"}>
-            <span className="required-input">Email Address</span>
-            <input type="email" name="email" required={true} />
-            {errorMessage.email}
-        </label>
-        <fieldset className={"wide"}>
-            <span className="required-input wide">Query Type</span>
-            <label className="split">
-                <input type="radio" name="queryType" value="general-enquiry" required={true} />
-                <span>General Inquiry</span>
-            </label>
-            <label className="split">
-                <input type="radio" name="queryType" value="support-request" />
-                <span>Support Request</span>
-            </label>
-            {errorMessage.queryType}
-        </fieldset>
-        <label className={"wide block"}>
-            <span className="required-input">Message</span>
-            <textarea name="message" required={true}></textarea>
-            {errorMessage.message}
-        </label>
-        <label className={"wide"}>
-            <input type="checkbox" name="consent" required={true} />
-            <span className="required-input">I consent to being contacted by the team</span>
-            {errorMessage.consent}
-        </label>
-        <button className="wide" type="submit" value="submit">Submit</button>
+  return (
+    <form
+      id="contact-form"
+      onSubmit={handleSubmit}
+      onInvalidCapture={handleInvalid}
+      onChangeCapture={handleChange}
+    >
+      <h2 className="wide">Contact Us</h2>
+      <InputField
+        inputType="name"
+        inputStyle="split block"
+        label="First Name"
+        name="firstName"
+        message={errorMessage}
+      />
+      <InputField
+        inputType="name"
+        inputStyle={"split block"}
+        label="Last Name"
+        name="lastName"
+        message={errorMessage}
+      />
+      <InputField
+        inputType="email"
+        inputStyle="wide block"
+        label="Email Address"
+        name="email"
+        message={errorMessage}
+      />
+      <InputField
+        inputType="selectionExclusive"
+        inputStyle="wide block"
+        label="Query Type"
+        name="queryType"
+        options={[
+            {text: "General Inquiry", value: "general-inquiry"},
+            {text: "Support Request", value: "support-request"},
+        ]}
+        message={errorMessage}
+      />
+      <InputField
+        inputType="textbox"
+        inputStyle="wide block"
+        label="Message"
+        name="message"
+        message={errorMessage}
+      />
+      <InputField
+        inputType="acknowledgement"
+        inputStyle="wide"
+        label="I consent to being contacted by the team"
+        name="consent"
+        message={errorMessage}
+      />
+      <button className="wide" type="submit" value="submit">
+        Submit
+      </button>
     </form>
-    )
+  );
 }
 export default ContactForm;
---
+++ src/components/InputField.jsx
@@ -0,0 +1,110 @@
+import PropTypes from "prop-types";
+
+function RequiredMarker() {
+  return <span style={{ marginLeft: 5 }}>*</span>;
+}
+function RadioButton({ text, value, name }) {
+  return (
+    <label style={{flexBasis: '50%'}}>
+      <input type="radio" name={name} value={value} required={true} />
+      <span>{text}</span>
+    </label>
+  );
+}
+export default function InputField({
+  inputType,
+  inputStyle,
+  label,
+  name,
+  message,
+  options,
+}) {
+  let inputField;
+
+  switch (inputType) {
+    case "name":
+      inputField = (
+        <label className={inputStyle}>
+          <span>
+            {label}
+            <RequiredMarker />
+          </span>
+          <input type="text" name={name} required={true} />
+          {message[name]}
+        </label>
+      );
+      break;
+
+    case "email":
+      inputField = (
+        <label className={inputStyle}>
+          <span>
+            {label}
+            <RequiredMarker />
+          </span>
+          <input type="text" name={name} required={true} />
+          {message[name]}
+        </label>
+      );
+      break;
+
+    case "selectionExclusive":
+      inputField = (
+        <fieldset style={{flexBasis: '100%'}}>
+          <span className="wide">
+            {label} <RequiredMarker />
+          </span>
+          {options.map((option) => {
+            return (
+              <RadioButton
+                key={option.value}
+                text={option.text}
+                value={option.value}
+              />
+            );
+          })}
+          {message[name]}
+        </fieldset>
+      );
+      break;
+
+    case "textbox":
+      inputField = (
+        <label className={inputStyle}>
+          <span>{label}<RequiredMarker /></span>
+          <textarea name={name} required={true}></textarea>
+          {message[name]}
+        </label>
+      );
+      break;
+
+    case "acknowledgement":
+      inputField = (
+        <label className={inputStyle}>
+        <input type="checkbox" name={name} required={true} />
+        <span>
+          {label} <RequiredMarker />
+        </span>
+        {message[name]}
+      </label>
+      )
+      break;
+
+    default:
+      break;
+  }
+  return inputField;
+}
+RadioButton.propTypes = {
+  text: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired
+}
+InputField.propTypes = {
+  inputType: PropTypes.string.isRequired,
+  inputStyle: PropTypes.string,
+  label: PropTypes.string,
+  name: PropTypes.string,
+  message: PropTypes.object,
+  options: PropTypes.array,
+}
