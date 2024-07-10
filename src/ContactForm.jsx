ff210fe6c884e6ad6d0d55a188ce0da51ef92a16
ephraim <github@beltran.cc>
2024-07-10 03:02:34 -0400

    refactor(form validation): finalize the form validation Forms will now display messages on invalid inputs and disappear on valid input change

--- src/ContactForm.jsx
+++ src/ContactForm.jsx
@@ -1,62 +1,72 @@
 import { useState } from "react";
+import styles from "./ContactForm.module.css";
 import "./ContactForm.css";
 
 function ContactForm() {
-    const [inputInvalid, setInputInvalid] = useState({});
+    const [errorMessage, setErrorMessage] = useState({});
+    const setMessage = (inputName, message) => {
+        setErrorMessage(currentMessages => ({
+            ...currentMessages,
+            [inputName]: <span>{message}</span>
+        }))
+    }
+    const validate = (target) => {
+        const {validity, name} = target;
+        let inputType = 'input'
+        let emptyInputMessage = 'This field is required';
+
+        if(target.type == 'email') inputType = 'email address';
+        if(target.name == 'consent') emptyInputMessage = 'To submit this form, please consent to being contacted';
+        if(target.name == 'queryType') emptyInputMessage = 'Please select a query type';
+
+        if(validity.valid)              return setMessage(name, null);
+
+        if(validity.badInput)           return setMessage(name, "Invalid input");
+        if(validity.patternMismatch)    return setMessage(name, "Invalid input");
+        if(validity.rangeOverflow)      return setMessage(name, "Input is higher than accepted value");
+        if(validity.rangeUnderflow)     return setMessage(name, "Input is lower than accepted value");
+        if(validity.stepMismatch)       return setMessage(name, "Invalid value");
+        if(validity.tooLong)            return setMessage(name, "Input is too long");
+        if(validity.tooShort)           return setMessage(name, "Input is too short");
+        if(validity.typeMismatch)       return setMessage(name, `Please entert a valid ${inputType}`);
+        if(validity.valueMissing)       return setMessage(name, emptyInputMessage);
+    }
     const handleInvalid = (event) => {
         event.preventDefault();
         const target = event.target;
         console.warn(`Invalid ${target.name} input`);
-        setInputInvalid((invalidInputs) => ({...invalidInputs, [target.name] : true}))
+        validate(target);
     }
+    const handleChange = ({target}) => {
+        validate(target);
+    }
+
     const handleSubmit = (event) => {
         event.preventDefault();
         const formData = new FormData(event.target);
-        for (const [name, value] of formData.entries()) {
-            if(value !== undefined | value.length > 0) {
-                setInputInvalid((invalidInputs) => ({...invalidInputs, [name]: false}));
-            }
-        }
+        // Handle form data here
+        console.dir(formData);
     }
-    const errorMessage = (type) => {
-        let message;
-        switch (type) {
-            case 'text':
-                message = <span className="error-message">This field is required</span>;
-                break;
-            case 'email':
-                message = <span className="error-message">Please enter a valid email address</span>;
-                break;
-            case 'consent':
-                message = <span className="error-message block">To submit this form, please consent to being contacted</span>;
-                break;
-            case 'query':
-                message = <span className="error-message">Please select a query type</span>;
-                break;
-            default:
-                break;
-        }
-        return message;
-    }
+
     return (
-    <form id="contact-form" onSubmit={handleSubmit} onInvalidCapture={handleInvalid}>
+    <form id="contact-form" onSubmit={handleSubmit} onInvalidCapture={handleInvalid} onChangeCapture={handleChange}>
         <h2 className="wide">Contact Us</h2>
-        <label className={(inputInvalid.firstName?"invalid-input ":"") + "split block"}>
+        <label className={"split block"}>
             <span className="required-input">First Name</span>
             <input type="text" name="firstName" required={true} />
-            {inputInvalid.firstName && errorMessage('text')}
+            {errorMessage.firstName}
         </label>
-        <label className={(inputInvalid.lastName?"invalid-input ":"") + "split block"}>
+        <label className={"split block"}>
             <span className="required-input">Last Name</span>
             <input type="text" name="lastName" required={true} />
-            {inputInvalid.lastName && errorMessage('text')}
+            {errorMessage.lastName}
         </label>
-        <label className={(inputInvalid.email?"invalid-input ":"") + "wide block"}>
+        <label className={"wide block"}>
             <span className="required-input">Email Address</span>
             <input type="email" name="email" required={true} />
-            {inputInvalid.email && errorMessage('email')}
+            {errorMessage.email}
         </label>
-        <fieldset className={(inputInvalid.queryType?"invalid-input ":"") + "wide"}>
+        <fieldset className={"wide"}>
             <span className="required-input wide">Query Type</span>
             <label className="split">
                 <input type="radio" name="queryType" value="general-enquiry" required={true} />
@@ -66,17 +76,17 @@ function ContactForm() {
                 <input type="radio" name="queryType" value="support-request" />
                 <span>Support Request</span>
             </label>
-            {inputInvalid.queryType && errorMessage('query')}
+            {errorMessage.queryType}
         </fieldset>
-        <label className={(inputInvalid.message?"invalid-input ":"") + "wide block"}>
+        <label className={"wide block"}>
             <span className="required-input">Message</span>
             <textarea name="message" required={true}></textarea>
-            {inputInvalid.message && errorMessage('text')}
+            {errorMessage.message}
         </label>
-        <label className={(inputInvalid.consent?"invalid-input ":"") + "wide"}>
+        <label className={"wide"}>
             <input type="checkbox" name="consent" required={true} />
             <span className="required-input">I consent to being contacted by the team</span>
-            {inputInvalid.consent && errorMessage('consent')}
+            {errorMessage.consent}
         </label>
         <button className="wide" type="submit" value="submit">Submit</button>
     </form>
