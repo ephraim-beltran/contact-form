b19c8b6ab9e4b3915082ede3b5761ab320419709
ephraim <github@beltran.cc>
2024-07-12 19:07:47 -0400

    style: provide more form styling

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -10,17 +10,17 @@ function InputField({ errorMessage }) {
     <label className={styled.entry}>
         <span className={styled.label}>First Name<RequiredMarker/></span>
         <input type="text" name="firstname" required={true} />
-        <span>{errorMessage.firstname}</span>
+        <span className={styled.error}>{errorMessage.firstname}</span>
     </label>
     <label className={styled.entry}>
         <span className={styled.label}>Last Name<RequiredMarker/></span>
         <input type="text" name="lastname" required={true} />
-        <span>{errorMessage.lastname}</span>
+        <span className={styled.error}>{errorMessage.lastname}</span>
     </label>
     <label className={`${styled.wide} ${styled.entry}`}>
         <span className={styled.label}>Email Address<RequiredMarker/></span>
         <input type="email" name="email" required={true} />
-        <span>{errorMessage.email}</span>
+        <span className={styled.error}>{errorMessage.email}</span>
     </label>
     <fieldset className={`${styled.entry} ${styled.selection}`}>
         <span>Query Type<RequiredMarker/></span>
@@ -32,17 +32,17 @@ function InputField({ errorMessage }) {
             <input type="radio" name="queryType" value="support-request" required />
             <span>Support Request</span>
         </label>
-        <span>{errorMessage.queryType}</span>
+        <span className={styled.error}>{errorMessage.queryType}</span>
     </fieldset>
     <label className={`${styled.wide} ${styled.entry}`}>
         <span>Message<RequiredMarker /></span>
         <textarea name="message" required></textarea>
-        <span>{errorMessage.message}</span>
+        <span className={styled.error}>{errorMessage.message}</span>
     </label>
     <label className={styled.wide}>
         <input type="checkbox" name="consent" required />
         <span>I consent to being contacted by the team<RequiredMarker/></span>
-        <span>{errorMessage.consent}</span>
+        <span className={styled.error}>{errorMessage.consent}</span>
     </label>
     </>
     )
