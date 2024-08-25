a7ed3d67cc2d5769190f8e347c0ac415e0ce9d75
ephraim <github@beltran.cc>
2024-08-25 00:05:54 -0400

    modified:   src/App.jsx new file:   src/components/Footer/index.jsx new file:   src/components/Header/index.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
 import ContactForm from "./ContactForm";
 function App() {
-
   return (
     <>
+      <Header />
       <ContactForm />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
