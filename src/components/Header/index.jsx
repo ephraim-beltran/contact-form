d50bc5bc6bba2cb00290fe9ef4a0b71b64a02240
ephraim <github@beltran.cc>
2024-08-25 00:13:06 -0400

    Create a header title

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+  return (
+    <header>
+      <h1 style={{ textAlign: "center", width: "100%" }}>
+        React Contact Form Component
+      </h1>
+    </header>
+  );
+};
